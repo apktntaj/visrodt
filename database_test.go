43ead593191c03b41a24747382061d910afff1ef
apktntaj <maiaelarsya@gmail.com>
2024-10-04 13:56:23 +0700

    Open and close connection

--- database_test.go
+++ database_test.go
@@ -1,6 +1,7 @@
-package visrodt
+package main
 
 import (
+	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -8,4 +9,13 @@ import (
 
 func TestEmpty(t *testing.T) {
 
+}
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "alamasyarie:sepertikemarin@tcp(localhost:3306)/visrodt")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
 }
